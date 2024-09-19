5cfba0d9a37a21f978e0e59ad3aa965dd93a579b
JawadTakiAlden <jawad.taki.alden2002@gmail.com>
2024-09-19 22:11:09 +0300

    add lesion

--- src/views/pages/courses/courseDetails/chaptersRenderer/components/AddLesionForm/index.js
+++ src/views/pages/courses/courseDetails/chaptersRenderer/components/AddLesionForm/index.js
@@ -80,6 +80,9 @@ const AddLesionForm = ({ chapter, handelClose }) => {
   });
   const videos = useGetVideos(inputValue ,values.source);
   
+  useEffect(() => {
+    videos.refetch(inputValue ,values.source)
+  } , [values.source])
 
   const handelRefetchOnSearch = useDebounce(() => {
     videos.refetch();
